9a34a54e672916a16b9fb6af6a29a05a175349a6
Lachlan Clulow <lachlanclulow@gmail.com>
2025-11-29 10:01:34 +1100

    setup for 2025

---
+++ 2025/12/main.py
@@ -0,0 +1,16 @@
+import pathlib
+
+
+def main():
+    puzzle_input = open(
+        pathlib.Path(__file__).parent.resolve().joinpath("input.txt"), "r"
+    ).read()
+
+    # I'd like to solve the puzzle
+    answer = 10
+
+    return(answer)
+
+
+if __name__ == "__main__":
+    print(main())
