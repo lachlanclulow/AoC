3b832dce074dc7ea4bf449000f4f5c0a47646880
Lachlan Clulow <lachlanclulow@gmail.com>
2025-12-13 12:42:59 +1100

    this was a troll

--- 2025/12/1.py
+++ 2025/12/1.py
@@ -5,7 +5,26 @@ def main(puzzle_input: str):
     # I'd like to solve the puzzle
     answer = 0
 
-    return(answer)
+    stuff = puzzle_input.split("\n\n")
+
+    shape_sizes  = {}
+    for shape_in in stuff[:-1]:
+        rows = shape_in.splitlines()
+        shape_sizes[int(rows[0].split(":")[0])] = 0
+        for r in rows[1:]:
+            ar = [1 if c == "#" else 0 for c in r]
+            shape_sizes[int(rows[0].split(":")[0])] += sum(ar)
+
+    for tree in stuff[-1].splitlines():
+        dims, req = tuple(tree.split(": "))
+        x, y = tuple([int(x) for x in dims.split("x")])
+        reqs = []
+        for i, d in enumerate(req.split()):
+            reqs += [i] * int(d)  
+
+        answer += sum([shape_sizes[x] for x in reqs]) < x*y
+
+    return answer
 
 
 if __name__ == "__main__":
